d6f8fc7026974a45faa95ebc1ff563a208953e52
Malyhin_Ilya <malyhin.ilya60@gmail.com>
2025-11-06 23:07:57 +0500

    New activities: MainActivity.kt SettingsActivity.kt SearchActivity.kt MediaGalleryActivity.kt

---
+++ app/src/main/java/ru/practicum/playlistmaker/MainActivity.kt
@@ -0,0 +1,32 @@
+package ru.practicum.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediastoreButton = findViewById<Button>(R.id.mediastore_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        searchButton.setOnClickListener { startActivity(Intent(this, SearchActivity::class.java))}
+        mediastoreButton.setOnClickListener { startActivity(Intent(this, MediaGalleryActivity::class.java)) }
+        settingsButton.setOnClickListener { startActivity(Intent(this, SettingsActivity::class.java)) }
+    }
+}
---
+++ app/src/main/java/ru/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,26 @@
+package ru.practicum.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val backButton = findViewById<Button>(R.id.back_to_main)
+        backButton.setOnClickListener { startActivity(Intent(this, MainActivity::class.java)) }
+    }
+}
