0b0aed9a18c3ec16afbf74ad75b832c17eba2c15
Malyhin_Ilya <malyhin.ilya60@gmail.com>
2026-02-16 16:19:14 +0500

    Спринт 14. Handler, Debounce, ProgressBar, MediaPlayer. Воспроизведение композиций

--- app/src/main/java/ru/practicum/playlistmaker/adapter/TrackAdapter.kt
+++ app/src/main/java/ru/practicum/playlistmaker/adapter/TrackAdapter.kt
@@ -2,6 +2,8 @@ package ru.practicum.playlistmaker.adapter
 
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -11,6 +13,8 @@ import ru.practicum.playlistmaker.R
 import ru.practicum.playlistmaker.layout.TrackViewHolder
 import ru.practicum.playlistmaker.model.Track
 import ru.practicum.playlistmaker.service.HistoryService
+import ru.practicum.playlistmaker.utils.Debouncer
+import ru.practicum.playlistmaker.utils.Debouncer.Companion.clickDebounce
 
 const val TRACK_KEY = "TRACK"
 
@@ -24,10 +28,12 @@ class TrackAdapter(private val tracks: List<Track>, private val historyService: 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         holder.bind(tracks[position])
         holder.itemView.setOnClickListener {
-            historyService.add(tracks[position])
-            val intent = Intent(holder.itemView.context, PlayerActivity::class.java)
-            intent.putExtra(TRACK_KEY, Gson().toJson(tracks[position]))
-            holder.itemView.context.startActivity(intent)
+            if (clickDebounce()) {
+                historyService.add(tracks[position])
+                val intent = Intent(holder.itemView.context, PlayerActivity::class.java)
+                intent.putExtra(TRACK_KEY, Gson().toJson(tracks[position]))
+                holder.itemView.context.startActivity(intent)
+            }
         }
     }
 
--- app/src/main/java/ru/practicum/playlistmaker/layout/PlayerActivity.kt
+++ app/src/main/java/ru/practicum/playlistmaker/layout/PlayerActivity.kt
@@ -1,6 +1,10 @@
 package ru.practicum.playlistmaker.layout
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -14,13 +18,17 @@ import com.google.gson.Gson
 import ru.practicum.playlistmaker.R
 import ru.practicum.playlistmaker.model.Track
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
 import java.util.Locale
 
 const val TRACK_KEY = "TRACK"
 
 class PlayerActivity : AppCompatActivity() {
-
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+    }
     private lateinit var trackName: TextView
     private lateinit var artist: TextView
     private lateinit var albumImg: ImageView
@@ -30,7 +38,23 @@ class PlayerActivity : AppCompatActivity() {
     private lateinit var duration: TextView
     private lateinit var country: TextView
     private lateinit var backButton: MaterialToolbar
+    private lateinit var playButton: ImageView
+    private lateinit var timePlayed: TextView
 
+    private lateinit var track: Track
+
+    private var handler: Handler? = null
+
+    private val runnable = object : Runnable {
+        override fun run() {
+            showPlayingPosition()
+            handler?.postDelayed(this, 300)
+        }
+    }
+
+    private var mediaPlayer = MediaPlayer()
+
+    private var playerState = STATE_DEFAULT
 
     private fun init() {
         trackName = findViewById(R.id.track_name)
@@ -45,6 +69,9 @@ class PlayerActivity : AppCompatActivity() {
         backButton = findViewById(R.id.back_to_main)
         backButton.setTitleTextAppearance(this, R.style.header_style)
         backButton.setNavigationOnClickListener { finish() }
+
+        playButton = findViewById(R.id.play)
+        timePlayed = findViewById(R.id.time_played)
     }
 
 
@@ -58,7 +85,31 @@ class PlayerActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val track = Gson().fromJson(intent.getStringExtra(TRACK_KEY), Track::class.java)
+        handler = Handler(Looper.getMainLooper())
+        track = Gson().fromJson(intent.getStringExtra(TRACK_KEY), Track::class.java)
+
+        showTrackInfo()
+        preparePlayer()
+
+        playButton.setOnClickListener {
+            if (playerState == STATE_PREPARED || playerState == STATE_PAUSED)
+                startPlayer()
+            else if (playerState == STATE_PLAYING)
+                pausePlayer()
+        }
+    }
+
+    private fun preparePlayer() {
+        mediaPlayer.setDataSource(track.previewUrl)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener { playerState = STATE_PREPARED }
+        mediaPlayer.setOnCompletionListener {
+            playerState = STATE_PREPARED
+            playButton.setImageResource(R.drawable.play)
+        }
+    }
+
+    private fun showTrackInfo() {
 
         Glide.with(albumImg)
             .load(track.artworkUrl100.replaceAfterLast('/',"512x512bb.jpg")).into(albumImg)
@@ -71,5 +122,34 @@ class PlayerActivity : AppCompatActivity() {
         genre.text = track.primaryGenreName
         duration.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTimeMillis)
         country.text = track.country
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        showPlayingPosition()
+        playerState = STATE_PLAYING
+        playButton.setImageResource(R.drawable.pause)
+
+        showPlayingPosition()
+
+        handler?.post(runnable)
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playerState = STATE_PAUSED
+        playButton.setImageResource(R.drawable.play)
+    }
+
+    private fun showPlayingPosition() {
+        Log.i("TrackPosition", "Current position is " + mediaPlayer.currentPosition / 60)
+        timePlayed.text = SimpleDateFormat("m:ss", Locale.getDefault()).format(mediaPlayer.currentPosition)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler?.removeCallbacks(runnable)
+        mediaPlayer.stop()
+        mediaPlayer.release()
     }
 }
